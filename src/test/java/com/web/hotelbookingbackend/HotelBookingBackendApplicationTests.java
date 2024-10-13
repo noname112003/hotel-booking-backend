c64f4c593a6b652ce80ffc2790c828946166e203
noname112003 <lvquang7112003@gmail.com>
2024-10-13 10:42:27 +0700

    code base

---
+++ src/test/java/com/web/hotelbookingbackend/HotelBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.hotelbookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
